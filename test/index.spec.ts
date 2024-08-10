1c95ac9eddec9987b9b7522798610a5c24c520eb
Matthew Miller <matthew@millerti.me>
2024-08-10 11:47:04 -0700

    Clean up tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,29 +1,8 @@
-// test/index.spec.ts
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
+import { SELF } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
 import { PublicKeyCredentialCreationOptionsJSON } from '@simplewebauthn/types';
-// import worker from '../src/index';
-
-// For now, you'll need to do something like this to get a correctly-typed
-// `Request` to pass to `worker.fetch()`.
-// const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
 describe('Routing tests', () => {
-	// it('responds with Hello World! (unit style)', async () => {
-	// 	const request = new IncomingRequest('http://example.com');
-	// 	// Create an empty context to pass to `worker.fetch()`.
-	// 	const ctx = createExecutionContext();
-	// 	const response = await worker.fetch(request, env, ctx);
-	// 	// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-	// 	await waitOnExecutionContext(ctx);
-	// 	expect(await response.text()).toMatchInlineSnapshot(`"Hello from passkeys.dev RP demo API!"`);
-	// });
-
-	// it('responds with Hello World! (integration style)', async () => {
-	// 	const response = await SELF.fetch('https://example.com');
-	// 	expect(await response.text()).toMatchInlineSnapshot(`"Hello from passkeys.dev RP demo API!"`);
-	// });
-
 	it('recognizes GET /registration/options', async () => {
 		const response = await SELF.fetch('https://example.com/registration/options?userName=foo');
 		expect(response.status).toBe(200);
